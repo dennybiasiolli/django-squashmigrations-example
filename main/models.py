545d0e880fb06fed90728c7915645ab8fdb79a79
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-08 09:41:26 +0100

    main: adding Like model

--- main/models.py
+++ main/models.py
@@ -6,3 +6,12 @@ class Tweet(models.Model):
     created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=250)
+
+
+class Like(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["tweet", "created_by"]

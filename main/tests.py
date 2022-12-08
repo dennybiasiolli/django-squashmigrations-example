0d46ae7c12bbbfbf25a4cbb39c872341fc13f98b
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-08 09:41:26 +0100

    main: adding Like model

--- main/tests.py
+++ main/tests.py
@@ -1,16 +1,18 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.db.utils import DataError
+from django.db import transaction
+from django.db.utils import DataError, IntegrityError
 from django.test import TestCase
 
-from main.models import Tweet
+from main.models import Like, Tweet
 
 
 class TweetTestCase(TestCase):
     def setUp(self):
         UserModel = get_user_model()
-        self.u1 = UserModel.objects.create()
+        self.u1 = UserModel.objects.create(username="foo")
+        self.u2 = UserModel.objects.create(username="bar")
         self.t1 = Tweet.objects.create(
             created_by=self.u1,
             text="My first tweet",
@@ -37,3 +39,25 @@ class TweetTestCase(TestCase):
             self.assertRaises(ValidationError, tweet.full_clean)
         elif db_engine == "django.db.backends.postgresql":
             self.assertRaises(DataError, create_fn)
+
+    def test_tweet_like_unique_per_user(self):
+        tweet = Tweet.objects.create(
+            created_by=self.u1,
+            text="a" * 250,
+        )
+        Like.objects.create(
+            tweet=tweet,
+            created_by=self.u1,
+        )
+        Like.objects.create(
+            tweet=tweet,
+            created_by=self.u2,
+        )
+        with self.assertRaises(Exception) as raised:
+            with transaction.atomic():
+                Like.objects.create(
+                    tweet=tweet,
+                    created_by=self.u2,
+                )
+        self.assertEqual(IntegrityError, type(raised.exception))
+        self.assertEqual(tweet.like_set.count(), 2)

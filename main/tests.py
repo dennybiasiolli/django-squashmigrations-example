6a74d3942f9dc3583099c5b0c9755234efe49034
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-08 09:41:26 +0100

    main: adding Like model

--- main/tests.py
+++ main/tests.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-from main.models import Tweet
+from main.models import Like, Tweet
 
 
 class TweetTestCase(TestCase):
     def setUp(self):
         UserModel = get_user_model()
-        self.u1 = UserModel.objects.create()
+        self.u1 = UserModel.objects.create(username="foo")
+        self.u2 = UserModel.objects.create(username="bar")
         self.t1 = Tweet.objects.create(
             created_by=self.u1,
             text="My first tweet",
@@ -28,3 +31,25 @@ class TweetTestCase(TestCase):
             text="a" * 281,
         )
         self.assertRaises(ValidationError, tweet.full_clean)
+
+    def test_tweet_like_unique_per_user(self):
+        tweet = Tweet.objects.create(
+            created_by=self.u1,
+            text="a" * 281,
+        )
+        Like.objects.create(
+            tweet=tweet,
+            created_by=self.u1,
+        )
+        Like.objects.create(
+            tweet=tweet,
+            created_by=self.u2,
+        )
+        with self.assertRaises(Exception) as raised:
+            with transaction.atomic():
+                Like.objects.create(
+                    tweet=tweet,
+                    created_by=self.u2,
+                )
+        self.assertEqual(IntegrityError, type(raised.exception))
+        self.assertEqual(tweet.like_set.count(), 2)

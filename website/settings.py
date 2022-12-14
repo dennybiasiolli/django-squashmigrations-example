36123bdfec99a9607f983c7d810ded8293197da2
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-14 10:31:07 +0100

    adding shop app
    `python manage.py startapp shop`

--- website/settings.py
+++ website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "main.apps.MainConfig",
+    "shop.apps.ShopConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

01f67fea7202e599ec1c11248e5f808618c1817b
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-14 21:19:27 +0100

    shop: removing Customer shipping fields

--- shop/models.py
+++ shop/models.py
@@ -10,12 +10,6 @@ class Customer(models.Model):
         related_name="customer",
     )
     is_premium = models.BooleanField(default=False)
-    shipping_name = models.CharField(max_length=100)
-    shipping_address = models.CharField(max_length=50)
-    shipping_zip_code = models.CharField(max_length=10)
-    shipping_city = models.CharField(max_length=50)
-    shipping_province = models.CharField(max_length=5)
-    shipping_state = models.CharField(max_length=20)
 
 
 class ShippingAddress(models.Model):

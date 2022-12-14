768b999f70445a39af5a2a21f4c53db0bc562882
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-14 21:58:10 +0100

    shop: adding Order base model

--- shop/models.py
+++ shop/models.py
@@ -22,3 +22,23 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=100)
     province = models.CharField(max_length=5)
     state = models.CharField(max_length=50)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="orders"
+    )
+    shipping_name = models.CharField(max_length=200, blank=True)
+    shipping_address = models.CharField(max_length=100, blank=True)
+    shipping_zip_code = models.CharField(max_length=10, blank=True)
+    shipping_city = models.CharField(max_length=100, blank=True)
+    shipping_province = models.CharField(max_length=5, blank=True)
+    shipping_state = models.CharField(max_length=50, blank=True)
+
+    def copy_shipping_address(self, shipping_address: ShippingAddress):
+        self.shipping_name = shipping_address.name
+        self.shipping_address = shipping_address.address
+        self.shipping_zip_code = shipping_address.zip_code
+        self.shipping_city = shipping_address.city
+        self.shipping_province = shipping_address.province
+        self.shipping_state = shipping_address.state

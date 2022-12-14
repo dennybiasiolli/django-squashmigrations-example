1ebe904b3beaeb8f145b26369c4ebc9fea172236
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-12-14 20:32:18 +0100

    shop: adding Customer model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        unique=True,
+        related_name="customer",
+    )
+    shipping_name = models.CharField(max_length=100)
+    shipping_address = models.CharField(max_length=50)
+    shipping_zip_code = models.CharField(max_length=10)
+    shipping_city = models.CharField(max_length=50)
+    shipping_province = models.CharField(max_length=5)
+    shipping_state = models.CharField(max_length=20)
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from shop.models import Customer
+
+
+class ShopTestCase(TestCase):
+    def setUp(self):
+        UserModel = get_user_model()
+        self.u1 = UserModel.objects.create(username="foo")
+        self.u2 = UserModel.objects.create(username="bar")
+
+    def test_customer_uniqueness_with_user(self):
+        Customer.objects.create(
+            user=self.u1,
+            shipping_name="1",
+            shipping_address="1",
+            shipping_zip_code="1",
+            shipping_city="1",
+            shipping_province="1",
+            shipping_state="1",
+        )
+        with self.assertRaises(Exception) as raised:
+            with transaction.atomic():
+                Customer.objects.create(
+                    user=self.u1,
+                    shipping_name="2",
+                    shipping_address="2",
+                    shipping_zip_code="2",
+                    shipping_city="2",
+                    shipping_province="2",
+                    shipping_state="2",
+                )
+        self.assertEqual(IntegrityError, type(raised.exception))
+        self.u1.refresh_from_db()
+        self.assertEqual(self.u1.customer.shipping_name, "1")
